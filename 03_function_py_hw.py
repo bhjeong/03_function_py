221c344da5347b2593ea4cf9bab39b90e02f104f
unknown <bhjeong@bhjeong.humaxdigital.com>
2015-06-10 17:16:36 +0900

    Initial commit

---
+++ 03_function_py_hw.py
@@ -0,0 +1,27 @@
+__author__ = 'bhjeong'
+
+
+print 100+200+300
+print 300-200
+
+def add(a,b):
+    return a+b
+
+def multi(c,d):
+    return c*d
+
+
+def substract(e,f):
+    return e-f
+
+def divide(g,h):
+    return g/h
+
+
+print add(100,200)
+print add('abc', 'def')
+print add("Today is", " Wednesday")
+print multi(200, 400)
+print substract(321, 123)
+print divide(777, 111)
+
